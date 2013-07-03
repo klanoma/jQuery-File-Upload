23dd6a87336c3c4d18fb3bb70875d5f645d3296f
William Youmans <will@williamyoumans.com>
2013-07-03 13:14:41 -0600

    cleanup

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [Demo File Upload](http://blueimp.github.io/jQuery-File-Upload/)
 
 ## Description
-File Upload widget with multiple file selection, drag&amp;drop support, progress bars, validation and preview images, audio and video for jQuery.  
+File Upload widget with multiple file selection, drag&amp;drop support, progress bars, validation and preview images, audio and video for jQuery.
 Supports cross-domain, chunked and resumable file uploads and client-side image resizing. Works with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads.
 
 ## Setup
@@ -13,47 +13,47 @@ Supports cross-domain, chunked and resumable file uploads and client-side image 
 
 ## Support
 
-* **[Support Forum](https://groups.google.com/d/forum/jquery-fileupload)**  
+* **[Support Forum](https://groups.google.com/d/forum/jquery-fileupload)**
 **Support requests** and **general discussions** about the File Upload plugin can be posted to the official
-[Support Forum](https://groups.google.com/d/forum/jquery-fileupload).  
+[Support Forum](https://groups.google.com/d/forum/jquery-fileupload).
 If your question is not directly related to the File Upload plugin, you might have a better chance to get a reply by posting to [Stack Overflow](http://stackoverflow.com/questions/tagged/blueimp+jquery+file-upload).
 
-* Bugs and Feature requests  
-**Bugs** and **Feature requests** can be reported using the [issues tracker](https://github.com/blueimp/jQuery-File-Upload/issues).  
+* Bugs and Feature requests
+**Bugs** and **Feature requests** can be reported using the [issues tracker](https://github.com/blueimp/jQuery-File-Upload/issues).
 Please read the [issue guidelines](https://github.com/blueimp/jQuery-File-Upload/blob/master/CONTRIBUTING.md) before posting.
 
 ## Features
-* **Multiple file upload:**  
+* **Multiple file upload:**
   Allows to select multiple files at once and upload them simultaneously.
-* **Drag & Drop support:**  
+* **Drag & Drop support:**
   Allows to upload files by dragging them from your desktop or filemanager and dropping them on your browser window.
-* **Upload progress bar:**  
+* **Upload progress bar:**
   Shows a progress bar indicating the upload progress for individual files and for all uploads combined.
-* **Cancelable uploads:**  
+* **Cancelable uploads:**
   Individual file uploads can be canceled to stop the upload progress.
-* **Resumable uploads:**  
+* **Resumable uploads:**
   Aborted uploads can be resumed with browsers supporting the Blob API.
-* **Chunked uploads:**  
+* **Chunked uploads:**
   Large files can be uploaded in smaller chunks with browsers supporting the Blob API.
-* **Client-side image resizing:**  
+* **Client-side image resizing:**
   Images can be automatically resized on client-side with browsers supporting the required JS APIs.
-* **Preview images, audio and video:**  
+* **Preview images, audio and video:**
   A preview of image, audio and video files can be displayed before uploading with browsers supporting the required APIs.
-* **No browser plugins (e.g. Adobe Flash) required:**  
+* **No browser plugins (e.g. Adobe Flash) required:**
   The implementation is based on open standards like HTML5 and JavaScript and requires no additional browser plugins.
-* **Graceful fallback for legacy browsers:**  
+* **Graceful fallback for legacy browsers:**
   Uploads files via XMLHttpRequests if supported and uses iframes as fallback for legacy browsers.
-* **HTML file upload form fallback:**  
+* **HTML file upload form fallback:**
   Allows progressive enhancement by using a standard HTML file upload form as widget element.
-* **Cross-site file uploads:**  
+* **Cross-site file uploads:**
   Supports uploading files to a different domain with cross-site XMLHttpRequests or iframe redirects.
-* **Multiple plugin instances:**  
+* **Multiple plugin instances:**
   Allows to use multiple plugin instances on the same webpage.
-* **Customizable and extensible:**  
+* **Customizable and extensible:**
   Provides an API to set individual options and define callBack methods for various upload events.
-* **Multipart and file contents stream uploads:**  
+* **Multipart and file contents stream uploads:**
   Files can be uploaded as standard "multipart/form-data" or file contents stream (HTTP PUT file upload).
-* **Compatible with any server-side application platform:**  
+* **Compatible with any server-side application platform:**
   Works with any server-side platform (PHP, Python, Ruby on Rails, Java, Node.js, Go etc.) that supports standard HTML form file uploads.
 
 ## Requirements
@@ -65,8 +65,8 @@ Please read the [issue guidelines](https://github.com/blueimp/jQuery-File-Upload
 * [JavaScript Canvas to Blob function](https://github.com/blueimp/JavaScript-Canvas-to-Blob) v. 2.0.6+ (optional)
 * [Bootstrap CSS Toolkit](https://github.com/twitter/bootstrap/) v. 2.3+ (optional)
 
-The jQuery UI widget factory is a requirement for the basic File Upload plugin, but very lightweight without any other dependencies.  
-The jQuery Iframe Transport is required for [browsers without XHR file upload support](https://github.com/blueimp/jQuery-File-Upload/wiki/Browser-support).  
+The jQuery UI widget factory is a requirement for the basic File Upload plugin, but very lightweight without any other dependencies.
+The jQuery Iframe Transport is required for [browsers without XHR file upload support](https://github.com/blueimp/jQuery-File-Upload/wiki/Browser-support).
 The UI version of the File Upload plugin also requires the JavaScript Templates engine as well as the JavaScript Load Image and JavaScript Canvas to Blob functions (for the image previews and resizing functionality). These dependencies are marked as optional, as the basic File Upload plugin can be used without them and the UI version of the plugin can be extended to override these dependencies with alternative solutions.
 
 The User Interface is built with Twitter's [Bootstrap](https://github.com/twitter/bootstrap/) Toolkit. This enables a CSS based, responsive layout and fancy transition effects on modern browsers. The demo also includes the [blueimp Gallery](https://github.com/blueimp/Gallery). Both of these components are optional and not required.
--- js/app.js
+++ js/app.js
@@ -12,103 +12,97 @@
 /*jslint nomen: true, regexp: true */
 /*global window, angular, navigator */
 
-(function () {
-    'use strict';
+(function() {
+  'use strict';
 
-    var isOnGitHub = window.location.hostname === 'blueimp.github.io',
-        url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'server/php/';
+  var isOnGitHub = window.location.hostname === 'blueimp.github.io',
+    url = isOnGitHub ? '//jquery-file-upload.appspot.com/' : 'server/php/';
 
-    angular.module('demo', [
-        'blueimp.fileupload'
-    ])
-        .config([
-            '$httpProvider', 'fileUploadProvider',
-            function ($httpProvider, fileUploadProvider) {
-                if (isOnGitHub) {
-                    // Demo settings:
-                    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-                    angular.extend(fileUploadProvider.defaults, {
-                        // Enable image resizing, except for Android and Opera,
-                        // which actually support image resizing, but fail to
-                        // send Blob objects via XHR requests:
-                        disableImageResize: /Android(?!.*Chrome)|Opera/
-                            .test(window.navigator && navigator.userAgent),
-                        maxFileSize: 5000000,
-                        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-                    });
-                }
+  angular.module('demo', [
+      'blueimp.fileupload'
+  ])
+    .config([
+      '$httpProvider', 'fileUploadProvider',
+    function($httpProvider, fileUploadProvider) {
+      if (isOnGitHub) {
+        // Demo settings:
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        angular.extend(fileUploadProvider.defaults, {
+          // Enable image resizing, except for Android and Opera,
+          // which actually support image resizing, but fail to
+          // send Blob objects via XHR requests:
+          disableImageResize: /Android(?!.*Chrome)|Opera/
+            .test(window.navigator && navigator.userAgent),
+          maxFileSize: 5000000,
+          acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+        });
+      }
+    }
+  ])
+
+  .controller('DemoFileUploadController', [
+      '$scope', '$http', '$filter', '$window',
+    function($scope, $http, $filter, $window) {
+      if (!isOnGitHub) {
+        $scope.loadingFiles = true;
+        $scope.options = {
+          url: url
+        };
+        $scope.display = function($event, file) {
+          var images = $filter('filter')($scope.queue, function(file) {
+            if (file.thumbnail_url) {
+              return true;
             }
-        ])
+          });
+          if ($window.blueimp.Gallery(images, {
+            index: file,
+            urlProperty: 'url',
+            titleProperty: 'name',
+            thumbnailProperty: 'thumbnail_url'
+          })) {
+            // Prevent the default link action on
+            // successful Gallery initialization:
+            $event.preventDefault();
+          }
+        };
+        $http.get(url)
+          .then(function(response) {
+          $scope.loadingFiles = false;
+          $scope.queue = response.data.files || [];
+        }, function() {
+          $scope.loadingFiles = false;
+        });
+      }
+    }
+  ])
 
-        .controller('DemoFileUploadController', [
-            '$scope', '$http', '$filter', '$window',
-            function ($scope, $http, $filter, $window) {
-                if (!isOnGitHub) {
-                    $scope.loadingFiles = true;
-                    $scope.options = {
-                        url: url
-                    };
-                    $scope.display = function ($event, file) {
-                        var images = $filter('filter')($scope.queue, function (file) {
-                            if (file.thumbnail_url) {
-                                return true;
-                            }
-                        });
-                        if ($window.blueimp.Gallery(images, {
-                                index: file,
-                                urlProperty: 'url',
-                                titleProperty: 'name',
-                                thumbnailProperty: 'thumbnail_url'
-                            })) {
-                            // Prevent the default link action on
-                            // successful Gallery initialization:
-                            $event.preventDefault();
-                        }
-                    };
-                    $http.get(url)
-                        .then(
-                            function (response) {
-                                $scope.loadingFiles = false;
-                                $scope.queue = response.data.files || [];
-                            },
-                            function () {
-                                $scope.loadingFiles = false;
-                            }
-                        );
-                }
-            }
-        ])
-
-        .controller('FileDestroyController', [
-            '$scope', '$http',
-            function ($scope, $http) {
-                var file = $scope.file,
-                    state;
-                if (file.url) {
-                    file.$state = function () {
-                        return state;
-                    };
-                    file.$destroy = function () {
-                        state = 'pending';
-                        return $http({
-                            url: file.delete_url,
-                            method: file.delete_type
-                        }).then(
-                            function () {
-                                state = 'resolved';
-                                $scope.clear(file);
-                            },
-                            function () {
-                                state = 'rejected';
-                            }
-                        );
-                    };
-                } else if (!file.$cancel && !file._index) {
-                    file.$cancel = function () {
-                        $scope.clear(file);
-                    };
-                }
-            }
-        ]);
+  .controller('FileDestroyController', [
+      '$scope', '$http',
+    function($scope, $http) {
+      var file = $scope.file,
+        state;
+      if (file.url) {
+        file.$state = function() {
+          return state;
+        };
+        file.$destroy = function() {
+          state = 'pending';
+          return $http({
+            url: file.delete_url,
+            method: file.delete_type
+          }).then(function() {
+            state = 'resolved';
+            $scope.clear(file);
+          }, function() {
+            state = 'rejected';
+          });
+        };
+      } else if (!file.$cancel && !file._index) {
+        file.$cancel = function() {
+          $scope.clear(file);
+        };
+      }
+    }
+  ]);
 
 }());
